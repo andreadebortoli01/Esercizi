2b22bd5d9ff150b3bba6a6e9441fbbab0490950a
andreadebortoli01 <andrea.debortoli16@gmail.com>
2020-10-13 22:19:44 +0200

    Es 2 somma e differenza caricato

---
+++ Es_2_Somma_Differenza_Ecc/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x = 0;
+    float y = 0;
+
+    cout <<"Inserire prima cifra  ";
+    cin >> x;
+    cout <<"Inserire seconda cifra  ";
+    cin >> y;
+
+    cout << "Somma="; cout << x+y; cout << "\n" ;
+    cout << "Differenza="; cout << x-y; cout << "\n" ;
+    cout << "Moltiplicazione="; cout << x*y; cout << "\n" ;
+    cout << "Divisione="; cout << x/y; cout << "\n" ;
+}
