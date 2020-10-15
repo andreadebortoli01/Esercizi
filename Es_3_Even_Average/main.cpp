b17219c67504ffb9146e89fe1afb751cb2a12927
andreadebortoli01 <andrea.debortoli16@gmail.com>
2020-10-15 15:39:37 +0200

    V1.1

--- Es_3_Even_Average/main.cpp
+++ Es_3_Even_Average/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
-
+                                //ESERCIZIO CHE CHIEDE NUMERI INTERI E CALCOLA LA MEIA DEI NUMERI PARI QUANDO
+                                //QUANDO L'UTENTE INSERISCE 0 CALCOLA IL RISULTATO
 int main()
 {
-    int IN ;
+    int IN ;                    //Inizializzazione variabili intere
     int sum=0 ;
     int n=0 ;
-    cout <<"Inserire numero \n";
-    cin >> IN ;
+
+    cout <<"Inserire numero ";  //Rischiesta primo numero
+    cin >> IN ;                 //
     
-    while(IN !=0) {
-        if(IN%2==0){
+    while(IN !=0) {             //Numero diverso da 0
+        if(IN%2==0){            //Aggiunta dei numeri pari alla media
             sum += IN;
             n++;
         }
-        cout <<"Inserire numero \n";
+        cout <<"Inserire numero ";  //Richiesta del numeri successivi da inserire alla media
         cin >> IN ;
     }
-    cout <<"Risultato " <<sum/n << "\n";
+    cout <<"Risultato " <<sum/n << "\n";    //Risultato della media
 }
